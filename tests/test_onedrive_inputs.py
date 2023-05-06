864201d3ebbf992dba43fc68ecabd7ff968f7d77
Itakello <maxste000@gmail.com>
2023-05-06 17:00:06 +0200

    Add test no links

--- tests/test_onedrive_inputs.py
+++ tests/test_onedrive_inputs.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 import azure.functions as func
@@ -6,23 +7,36 @@ from onedrive_file_processor import main
 
 
 class TestFunction(unittest.TestCase):
+    
     def test_no_params(self):
         req = func.HttpRequest(
             method='POST',
-            body=None,
+            body=b'{}',
             url='/api/onedrive_file_processor'
         )
         resp = main(req)
         self.assertEqual(resp.status_code, 200)
+        
     def test_multitple_links(self):
         links = [
             "https://1drv.ms/u/s!AtXdrMCFZ47igcl8Ox8oc3OAc71JZA",
             "https://1drv.ms/u/s!AtXdrMCFZ47igcl95UePK8v-15RPBQ",
             "https://1drv.ms/u/s!AtXdrMCFZ47igcl-xv_c5E_4NDOx8A"
         ]
+        body = json.dumps({"links": links}).encode()
         req = func.HttpRequest(
             method='POST',
-            body={"links": links},
+            body=body,
+            url='/api/onedrive_file_processor'
+        )
+        resp = main(req)
+        self.assertEqual(resp.status_code, 200)
+        
+    def test_zero_links(self):
+        body = json.dumps({"links": []}).encode()
+        req = func.HttpRequest(
+            method='POST',
+            body=body,
             url='/api/onedrive_file_processor'
         )
         resp = main(req)
